6765fef3515412c583e2021fb34de48ca95ebed5
Sergei Eremkin <ffr@list.ru>
2022-10-01 01:07:51 +0300

    слил ветку с логером, добавил комменты

--- operations.py
+++ operations.py
@@ -1,59 +1,57 @@
+from ast import Dict
 import json
 import csv
 
 from typing import List
-
+'''
+глобальный список для записи контактов. 
+'''
 global contact_list
 contact_list = []
 
 
 def add_contact(contact: List) -> None:
     '''
-    Запись в csv фаил. Этот же метод добавляет контакт. На дозвпись в файл mode_type = 'а', на перезапись файла = 'w'
+    добавить в список контакт
     '''
     contact_list.append(contact)
 
 
-#add_contact('sdsd,sdsd,dsd,dsd')
-
-
-def search_contact(searchstring: str) -> list:
+def search_contact(searchstring: str) -> List:
     '''
     Поиск в телефонной книге
     '''
     searched_contact = {}
     for contact in contact_list:
         if searchstring in contact:
-           searched_contact[contact_list.index(contact)] = contact
+            searched_contact[contact_list.index(contact)] = contact
     return searched_contact
-#list_l = [(1 ,['asdd','dsd','sdsd','sdsd'])]  
 
 
-def select_contact(choice, searched_contacts):
+def select_contact(choice: str, searched_contacts: Dict) -> Dict:
+    '''
+    Выбрать элемент в найденных
+    '''
     for key, value in searched_contacts.items():
         if choice == key:
-            return key, value    
-#print(select_contact(1, list_l))
+            return key, value
+
 
 def delete_contact(contact: str) -> None:
     '''
-    Ищем контакт и удаляем его из списка. Перезаписываем файл
+    Удалить контакт из списка 
     '''
     contact_list.remove(contact_list[contact])
 
 
 def edit_contact(index, value) -> None:
     '''
-    Редактирование контакта. На вход метод принимает поисковую строку для контакта и измененную строку. 
-    После перезаписываем файл с новыми измененниям
+    Редактирование контакта. 
     '''
     contact_list[index] = value
 
 
-# add_contact('sdsd,sdsd,dsd,dsd')
-
-
-def read_csv():
+def read_csv() -> None:
     '''
     Чтение из файла csv
     '''
@@ -61,47 +59,25 @@ def read_csv():
     with open('data.csv', encoding='utf-8') as f:
         reader = csv.reader(f, delimiter=',')
         for line in reader:
-            #line = ', '.join(line)
-            contact_list.append(line)
-       
 
-# print(read_csv())
+            contact_list.append(line)
 
 
 def write_csv() -> None:
     '''
-    Запись в csv фаил. Этот же метод добавляет контакт. На дозвпись в файл mode_type = 'а', на перезапись файла = 'w'
+    Запись в csv фаил. 
     '''
     with open('data.csv', 'w', encoding='utf-8') as f:
         writer = csv.writer(f, lineterminator='\r')
         for contact in contact_list:
             writer.writerow(contact)
-# phone_book_write_csv(phone_book)
-
-    # actual_contact_list =[]
-    # for contact in contact_list:
-    #     actual_contact_list.append(contact[2])
-    # actual_contact_list.append(line)
-
-
-#contact = 'dfdfd,dfdf,dfdf'
-# print(add_contact_csv(contact))
-
-
-# contact_list = read_csv()
-# # print(len(contact_list))
-# print(range(1, len(contact_list))+1)
-
-
-
-#print(select_contact('2', contact_list))
 
 
 def write_json() -> None:
     '''
     Вызвать метод для первой записи в файле
     '''
-    
+
     with open('data.json', 'w', encoding='utf-8', newline='') as rf:
         json.dump(contact_list, rf, ensure_ascii=False, indent=2)
 
